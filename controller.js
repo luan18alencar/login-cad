c9cdcdc795d2ae6e9e3a8f811275c3bbbae9a840
luan18alencar <luan.alencar@alunoescolassociais.org.br>
2024-08-08 08:11:32 -0300

    Apaga e recomeça JavaScript

--- controller.js
+++ controller.js
@@ -1,51 +1,52 @@
-function acessar(){
     // CRIA DUAS VARIAVEIS
+function acessar(){
     let loginEmail = document.getElementById('loginEmail').value;
     let loginSenha = document.getElementById('loginSenha').value;
     // PRIMEIRO IF FEITO PARA NÃO DEIXAR OS CAMPOS VAZIOS E RETORNAR UMA MENSAGEM AVISANDOQUE NÃO FORAM PREENCHIDOS
+ 
     if(!loginEmail || !loginSenha){
-        alert ('Favor preencher todos os campos');
+        alert('Favor preencher todos os campos');
     }else{
-    //alert('Campos preenchidos com sucesso');
-    //CONECTA O HTML AO JAVASCRIPT
+        alert('Campos preenchidos com sucesso');
+       
         window.location.href = 'cadastro.html';
     }
 }
-    // FUNÇÃO DE CRIAÇÃO DE ARRAY PARA ARMAZENAMENTO DE NOMES
-    var dadosLista = [];
-    // CRIA UMA FUNÇÃO PARA SALVAR USUÁRIO
-    function salvarUser(){
-    // CRIA UMA VARIÁVEL CHAMADA NOME DE USUÁRIO PARA SALVAR O NOME DE USUÁRIO
-    let nomeUser = document.getElementById('nomeUser').value;
-    // SEGUNDO IF FEITO PARA NÃO DEIXAR O CAMPO NOME DE USUÁRIO VAZIO 
-    if(nomeUser){
-    // O MÉTODO PUSH É UMA FUNÇÃO QUE PERMITE ADICIONAR UM ELEMENTO OU VÁRIOS AO FINAL DE UM ARRAY JÁ EXISTENTE
-        dadosLista.push(nomeUser);
-    // EXISTE ESSENCIALMENTE PARA PERMITIR O ENVIO DE DADOS PARA A CONSOLA DE DEPUTAÇÃO DO NAVEGADOR.
-    
-        console.log(dadosLista);
-        document.getElementById("nomeUser").value = "";
+ 
+ 
 
-    }else{
-        alert('Favor, informar um nome para cadastro');
+
+    // FUNÇÃO DE CRIAÇÃO DE ARRAY PARA ARMAZENAMENTO DE NOMES 
+var dadosLista = [];
+ 
+function salvarUser(){
+        // SEGUNDO IF FEITO PARA NÃO DEIXAR O CAMPO NOME DE USUÁRIO VAZIO
+    let nomeUser = document.getElementById('nomeUser').value;
+    if(nomeUser){
+         
+        // O MÉTODO PUSH É UMA FUNÇÃO QUE PERMITE ADICIONAR UM ELEMENTO OU VÁRIOS AO FINAL DE UM ARRAY JÁ EXISTENTE
+        dadosLista.push(nomeUser);
+        // EXISTE ESSENCIALMENTE PARA PERMITIR O ENVIO DE DADOS PARA A CONSOLA DE DEPUTAÇÃO DO NAVEGADOR.
+        console.log(dadosLista);
+        criarLista();
+        document.getElementById('nomeUser').value = "";
+    }
+    else{
+        alert("Favor informar o nome");
     }
 }
-    //  FUNÇÃO DE CRIAÇÃO DE LISTA
-    function criaLista(){
+    // FUNÇÃO DE CRIAÇÃO DE LISTA
+function criarLista(){
+
     // VARIAVEL TABELA ESTA A GUARDAR O CABEÇALHO
     let tabela = document.getElementById('tabela').innerHTML = "<tr><th>Nome Usuário</th><th>Ações</th></tr>";
     // CRIA LAÇO DE REPETIÇÃO FOR
     // LENGTH AUMENTA O TAMANHO DA LISTA
-    for(let i=0; i <= (dadosLista.length -1); i++){
+    for(let i = 0; i <= (dadosLista.length -1); i++){ 
     // TR CRIA LINHA NA TABELA
     // TD CRIA COLUNA
     // (+=) MANTÉM O QUE JA TEM E ADICIONA EM BAIXO
-        tabela += "<tr><td>" + dadosLista[I] + "</td><td></tr><tr>";
-        document.getElementById('tabela').innerHTML=
-        tabela;
+        tabela += "<tr><td>" + dadosLista[i] + "</td><td></td></tr>";
+        document.getElementById('tabela').innerHTML = tabela;
     }
 }
-
-
-
-//
