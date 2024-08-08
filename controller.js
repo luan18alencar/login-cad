2cba8d725a9c6bec3bb1571d8396fe77389a37f0
luan18alencar <luan.alencar@alunoescolassociais.org.br>
2024-08-08 07:32:14 -0300

    Cria laço for no projeto

--- controller.js
+++ controller.js
@@ -1,26 +1,34 @@
 function acessar(){
+    // CRIA DUAS VARIAVEIS
     let loginEmail = document.getElementById('loginEmail').value;
     let loginSenha = document.getElementById('loginSenha').value;
-
+    // Primeiro if feito para não deixar os campos vazios e retornar uma mensagem avisando que não foram preenchidos
     if(!loginEmail || !loginSenha){
         alert ('Favor preencher todos os campos');
     }else{
-        //alert('Campos preenchidos com sucesso');
+    //alert('Campos preenchidos com sucesso');
         window.location.href = 'cadastro.html';
     }
 }
-
-var dadosLista = [];
-
-function salvarUser(){
+    // FUNÇÃO DE CRIAÇÃO DE ARRAY PARA ARMAZENAMENTO DE NOMES
+    var dadosLista = [];
+    //
+    function salvarUser(){
     let nomeUser = document.getElementById('nomeUser').value;
     
     if(nomeUser){
         dadosLista.push(nomeUser);
         console.log(dadosLista);
-        document.getElementById(      "nomeUser").value = "";
+        document.getElementById("nomeUser").value = "";
 
     }else{
         alert('Favor, informar um nome para cadastro');
+    }
+}
+    //  FUNÇÃO DE CRIAÇÃO DE LISTA
+    function criaLista(){
+    let tabela = document.getElementById('tabela').innerHTML = "<tr><th>Nome Usuário</th><th>Ações</th></tr>";
+    for(let i=0; i <= (dadosLista.length -1); i++){
+
     }
 }
